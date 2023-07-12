65a8d4a8c592478d86a286fcaf7fddb779776bad
Simranjeet Singh <sin12743@sheridancollege.ca>
2023-07-12 19:43:51 -0400

    update codeee

--- src/main/java/sin12743/securityexample1/security/SecurityConfig.java
+++ src/main/java/sin12743/securityexample1/security/SecurityConfig.java
@@ -2,12 +2,15 @@ package sin12743.securityexample1.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
@@ -25,11 +28,47 @@ public class SecurityConfig {
                 .roles("USER", "PICKLE")//ALWAYS UPPERCASE
                 .build();
 
-        return new InMemoryUserDetailsManager(user1,user2);
+        return new InMemoryUserDetailsManager(user1, user2);
     }
-    @Bean PasswordEncoder passwordEncoder(){
+
+    @Bean
+    PasswordEncoder passwordEncoder() {
         PasswordEncoder encoder =
                 PasswordEncoderFactories.createDelegatingPasswordEncoder();
-        return encoder ;
+        return encoder;
     }
+
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+
+        http.authorizeHttpRequests((auth)->
+                auth
+                        .requestMatchers(HttpMethod.GET, "/").permitAll()
+                        .requestMatchers(HttpMethod.GET, "/user").hasRole("USER")
+                        .requestMatchers(HttpMethod.GET, "/Multiple").hasRole("PICKLE")
+                        .anyRequest().authenticated())
+                .formLogin((formlogin)->
+                formlogin
+                        .loginPage("/logintem")
+                        .failureUrl("/logintem?failed")
+                        .permitAll()
+
+                        )
+                .logout((logout)->
+                            logout
+                                    .deleteCookies("remove")
+                                    .invalidateHttpSession(true)
+                                    .logoutUrl("/logout")
+                                    .logoutSuccessUrl("/logintem?logout")
+                                    .permitAll())
+                .exceptionHandling((exceptionHandling)->
+                        exceptionHandling
+                                .accessDeniedPage("/accessdenied"));
+
+
+        return http.build();
+
+    }
+
 }
